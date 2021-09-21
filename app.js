3d7a7fb97f734b69a04d75cee09a8c7b052b37b4
preetimath <mathpreeti24@gmail.com>
2021-09-21 17:18:04 +0530

    import javascrpit

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("its working from different file");
+
+
+//input
+var username = prompt("give me ursername")
+
+alert("this script is working " + username);
+
+//processing
+var welcomemessage = "this script works!!" + username;
+
+//output
+alert(welcomemessage);
