6323856e45d8bb5be9ce9017e9143612ef92b427
preetimath <mathpreeti24@gmail.com>
2021-09-21 18:46:32 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-console.log("its working from different file");
+//console.log("its working from different file");
 
 
 //input
-var username = prompt("give me ursername")
+//var username = prompt("give me ursername")
 
-alert("this script is working " + username);
+//alert("this script is working " + username);
 
 //processing
-var welcomemessage = "this script works!!" + username;
+//var welcomemessage = "this script works!!" + username;
 
 //output
-alert(welcomemessage);
+//alert(welcomemessage);
+
+
+
+var btntranslate = document.querySelector("#btn-translate");
+var TxtInput = document.querySelector("#txt-input");
+console.log(TxtInput)
+
+function clickHandler(){
+
+    console.log("clicked");
+    console.log("input",TxtInput.value);
+};
+
+btntranslate.addEventListener("click", clickHandler)
+ 
+
+
+//  console.log("clicked")
+//})
+
